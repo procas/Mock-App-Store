0f3351b5c4ac8d9c00639cf88e4c813e1493b47a
Proma Mukherjee <Proma_Mukherjee@Dell.com>
2022-09-29 18:41:11 +0530

    Initial commit

---
+++ AppsControl/UserControls/AnApp.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppsControl.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AnApp.xaml
+    /// </summary>
+    public partial class AnApp : UserControl
+    {
+        private TextBox textBox;
+        String path;
+        private List<string> filepaths;
+        private Random random;
+        public AnApp()
+        {
+            InitializeComponent();
+            AppInfo.Foreground = Brushes.White;
+            path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString() + @"\Images";
+            filepaths = Directory.GetFiles(path, "*.png").ToList<string>();
+            FileInfo defaultImage = new FileInfo(filepaths[0]);
+            //ProductImage.Source = new BitmapImage(new Uri(defaultImage.FullName, UriKind.RelativeOrAbsolute));
+            //ProductName.Text = "calendar";
+            random = new Random();
+            FileInfo myRandomFile = new FileInfo(filepaths[random.Next(filepaths.Count)]);
+            ProductImage.Source = new BitmapImage(new Uri(myRandomFile.FullName, UriKind.RelativeOrAbsolute));
+            ProductName.Text = myRandomFile.Name.Split(".")[0];
+        }
+
+        
+    }
+}
---
+++ AppsControl/UserControls/AppsViewer.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppsControl.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AppsViewer.xaml
+    /// </summary>
+    public partial class AppsViewer : UserControl
+    {
+        private List<AnApp> apps;
+        private int widthOfAnApp;
+        public AppsViewer()
+        {
+            InitializeComponent();
+            apps = new();
+            AppsList.ItemsSource = apps;
+            for(int i = 0; i < 5; i++)
+            {
+                AnApp anApp = new AnApp();
+                apps.Add(anApp);
+            }
+            widthOfAnApp = (int)apps.First().ActualWidth + (int) apps.First().Margin.Left*2;
+        }
+
+        private void ScrollLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 4 * widthOfAnApp);
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 11);
+            //MessageBox.Show("CLICK WORKS!");
+        }
+        private void ScrollRightButton_Click(object sender, RoutedEventArgs e)
+        {
+            //AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 4 * widthOfAnApp);
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 11);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("CLICK WORKS");
+        }
+    }
+}
