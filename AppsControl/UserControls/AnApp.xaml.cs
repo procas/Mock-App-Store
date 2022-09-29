8f6fb247036a5cc64b3f8b82b07f24d3be3b5724
Proma Mukherjee <Proma_Mukherjee@Dell.com>
2022-09-29 23:40:14 +0530

    Inventory management added

--- AppsControl/UserControls/AnApp.xaml.cs
+++ AppsControl/UserControls/AnApp.xaml.cs
@@ -25,11 +25,15 @@ namespace AppsControl.UserControls
         String path;
         private List<string> filepaths;
         private Random random;
+        private int cost;
+        private int available_balance;
+        private HashSet<string> purchased_apps;
         public AnApp()
         {
             InitializeComponent();
             AppInfo.Foreground = Brushes.White;
             path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString() + @"\Images";
+            Download.Source = new BitmapImage(new Uri(path + "\\download.png", UriKind.RelativeOrAbsolute));
             filepaths = Directory.GetFiles(path, "*.png").ToList<string>();
             FileInfo defaultImage = new FileInfo(filepaths[0]);
             //ProductImage.Source = new BitmapImage(new Uri(defaultImage.FullName, UriKind.RelativeOrAbsolute));
@@ -38,8 +42,30 @@ namespace AppsControl.UserControls
             FileInfo myRandomFile = new FileInfo(filepaths[random.Next(filepaths.Count)]);
             ProductImage.Source = new BitmapImage(new Uri(myRandomFile.FullName, UriKind.RelativeOrAbsolute));
             ProductName.Text = myRandomFile.Name.Split(".")[0];
+            int price = random.Next(1, 100);
+            AppInfo.Content = "$"+price;
+            cost = price;
+            purchased_apps = new HashSet<string>();
+            
         }
 
-        
+        private void Download_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            available_balance = AppsViewer.MAXIMUM_BALANCE -= cost;
+            if (available_balance < 0)
+            {
+                //This item was not bought
+                MessageBox.Show("Oops, you have insufficient balance to buy this product! 👀");
+                AppsViewer.MAXIMUM_BALANCE += cost;
+            }
+            else if (purchased_apps.Contains(ProductName.Text))
+                MessageBox.Show("You have already purchased " + ProductName.Text + "!");
+            else
+            {
+                MessageBox.Show(ProductName.Text + " purchased worth " + AppInfo.Content + "!\nRemaining balance: " + available_balance);
+                purchased_apps.Add(ProductName.Text);
+            }
+        }
     }
 }
--- AppsControl/UserControls/AppsViewer.xaml.cs
+++ AppsControl/UserControls/AppsViewer.xaml.cs
@@ -22,30 +22,31 @@ namespace AppsControl.UserControls
     {
         private List<AnApp> apps;
         private int widthOfAnApp;
+        public static int MAXIMUM_BALANCE = 100; //dollars
         public AppsViewer()
         {
             InitializeComponent();
             apps = new();
             AppsList.ItemsSource = apps;
-            for(int i = 0; i < 5; i++)
+            for(int i = 0; i < 10; i++)
             {
                 AnApp anApp = new AnApp();
                 apps.Add(anApp);
             }
-            widthOfAnApp = (int)apps.First().ActualWidth + (int) apps.First().Margin.Left*2;
+            widthOfAnApp = (int)apps.First().Width;
         }
 
         private void ScrollLeftButton_Click(object sender, RoutedEventArgs e)
         {
 
             //AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 4 * widthOfAnApp);
-            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 11);
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 1*widthOfAnApp);
             //MessageBox.Show("CLICK WORKS!");
         }
         private void ScrollRightButton_Click(object sender, RoutedEventArgs e)
         {
             //AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 4 * widthOfAnApp);
-            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 11);
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 1*widthOfAnApp);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
